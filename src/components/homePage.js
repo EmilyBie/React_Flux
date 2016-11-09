d1509113a46b8185f4e5ab65ff539b700d798e95
emily <bieyuyu@gmail.com>
2016-11-09 11:08:31 +1100

    First commit: Environment Setup and Create homePage component

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var HomePage = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight Administration</h1>
+        <p>React and React Router responsive web app</p>
+      </div>
+    );
+  }
+});
+
+module.exports = HomePage;
